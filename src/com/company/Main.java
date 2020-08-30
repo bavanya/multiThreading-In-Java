0b60f50d6f90f9e3523dd926a9f8aec4ca497b1e
bavanya <kurrabchoudhry.cse18@itbhu.ac.in>
2020-08-31 01:33:57 +0530

    add single process and time taken

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,24 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        long number;
+        long noOfTimes;
+        int noOfThreads;
+        int countOfThreads;
+        Scanner input = new Scanner(System.in);
+        number = input.nextLong();
+        noOfTimes = input.nextLong();
+
+        long startTime1 = System.currentTimeMillis();
+        Skeleton skeleton = new Skeleton(number,noOfTimes);
+        for(long i = 1 ; i <= skeleton.getNoOfTimes() ; i++)
+            System.out.println(skeleton.getNumber() + " times " + i + " is " + skeleton.getMultiple(i));
+        System.out.println('\n');
+        long endTime1 = System.currentTimeMillis();
+        float timeTaken1 = (endTime1 - startTime1) / 1000F;
+        System.out.println("Time taken for single process is" + timeTaken1 + " seconds");
+        //TODO: Add Multithreading.
+
+
     }
 }
