1066ddedace0bfec21f6af179b78b7b1f4ecb3b9
bavanya <kurrabchoudhry.cse18@itbhu.ac.in>
2020-08-31 18:11:37 +0530

    add multithreading

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,12 +14,60 @@ public class Main {
 
         long startTime1 = System.currentTimeMillis();
         Skeleton skeleton = new Skeleton(number,noOfTimes);
-        for(long i = 1 ; i <= skeleton.getNoOfTimes() ; i++)
+        for( long i = 1 ; i <= skeleton.getNoOfTimes() ; i++ )
             System.out.println(skeleton.getNumber() + " times " + i + " is " + skeleton.getMultiple(i));
         System.out.println('\n');
         long endTime1 = System.currentTimeMillis();
         float timeTaken1 = (endTime1 - startTime1) / 1000F;
         System.out.println("Time taken for single process is" + timeTaken1 + " seconds");
-        //TODO: Add Multithreading.
+
+        number = input.nextLong();
+        noOfTimes = input.nextLong();
+        noOfThreads = input.nextInt();
+        countOfThreads = noOfThreads - 1;
+        SkeletonMultiThreaded[] skeletonMultiThreaded = new SkeletonMultiThreaded[noOfThreads];
+        Thread[] t = new Thread[noOfThreads];
+
+        long startTime2 = System.currentTimeMillis();
+        for( long i = 1 ; i <= noOfTimes ; i++ ){
+            if(countOfThreads != -1){
+                skeletonMultiThreaded[noOfThreads - countOfThreads - 1] = new SkeletonMultiThreaded(number,i);
+                countOfThreads--;
+            }
+            if(countOfThreads == -1) {
+                for( int j = 0; j < noOfThreads ; j++){
+                    t[j] = new Thread(skeletonMultiThreaded[j]);
+                    t[j].start();
+                }
+                for( int j = 0; j < noOfThreads ; j++ ){
+                    try {
+                        t[j].join();
+                        System.out.println(skeletonMultiThreaded[j].getNumber() + " times " + skeletonMultiThreaded[j].getMultiple()+ " is " + skeletonMultiThreaded[j].getResult());
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                countOfThreads = noOfThreads - 1;
+            }
+        }
+
+        if( countOfThreads != noOfThreads - 1 ){
+            for( int j = 0; j <noOfThreads - countOfThreads-1 ; j++ ){
+                t[j] = new Thread(skeletonMultiThreaded[j]);
+                t[j].start();
+            }
+            for( int j = 0; j < noOfThreads - countOfThreads - 1 ; j++ ){
+                try {
+                    t[j].join();
+                    System.out.println(skeletonMultiThreaded[j].getNumber() + " times " + skeletonMultiThreaded[j].getMultiple()+ " is " + skeletonMultiThreaded[j].getResult());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        long endTime2 = System.currentTimeMillis();
+
+        float timeTaken2 = (endTime2 - startTime2) / 1000F;
+        System.out.println("Time taken for multiThreaded process is" + timeTaken2 + " seconds");
     }
 }
